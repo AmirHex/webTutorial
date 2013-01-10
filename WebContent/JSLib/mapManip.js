15c8b92884aa87dcfdb351e0a150aa7346552ff2
Amir <amir@cs.toronto.edu>
2013-01-10 18:37:28 -0500

    Map implementation.

--- WebContent/JSLib/mapManip.js
+++ WebContent/JSLib/mapManip.js
@@ -11,16 +11,36 @@ function getAddress( address ){
 }
 
 function showStaticAddress( address ) {
-	var myCenter=new google.maps.LatLng(43,-80);
+	var mycenter=new google.maps.LatLng( 43.65960972192819, -79.39695954322815);
 	var mapOptions = {
-	  zoom: 8,
-	  center: new google.maps.LatLng(43, -80),
+	  zoom: 12,
+	  center: mycenter,
+	  disableDefaultUI: true,
 	  mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
+	
 	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	
+	
+	
+	//marker.setMap(map);
+	
+	var infowindow = new google.maps.InfoWindow({
+		content:"Take Elevator to fifth floor, and " +
+				"look for a half circle in the main " +
+				"hall!.",
+		maxWidth: "10px"
+	});
+	
+	
 	var marker = new google.maps.Marker({
-		position:myCenter
-		});
-
-	marker.setMap(map);
+		position:mycenter,
+		map: map
+	});
+	
+	//infowindow.open(map,marker);
+	
+	google.maps.event.addListener(marker, 'click', function(){
+		  infowindow.open(map,marker);
+	});
 }
