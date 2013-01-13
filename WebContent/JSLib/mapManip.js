7dd19ed17a81a0592ec74254ed75f8a6350d9b0a
AmirHex <amir@cs.toronto.edu>
2013-01-13 17:58:47 -0500

    set up map

--- WebContent/JSLib/mapManip.js
+++ WebContent/JSLib/mapManip.js
@@ -1,33 +1,68 @@
+/*
+ * Purpose: Initialize a google map on a <div> element 
+ * and show the location with a given latitude and longitude on it.
+ * 
+ * Input Arguments:
+ * -None.
+ * 
+ * Output Arguments:
+ * -None.
+ * 
+ */
+function initMap(){
+	//alert('Inside init');
+	var address={'LatLng':new google.maps.LatLng( 43.65960972192819, -79.39695954322815)};
+	showStaticAddress( 'map', address );
+}
 
+
+/*
+ * Purpose: adds a google map's api to the webpage. 
+ * 
+ * Input Argument: 
+ * -None. 
+ * 
+ * Output Arguments:
+ * -None.
+ */
 function loadMapLibrary() {
   var script = document.createElement("script");
   script.type = "text/javascript";
-  script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyBciReWu1vsS0vcpruteCA48pC8Cs0CYok&sensor=false&callback=showStaticAddress";
+  script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyBciReWu1vsS0vcpruteCA48pC8Cs0CYok&sensor=false&callback=initMap";
   document.body.appendChild(script);
 }
 
-function getAddress( address ){
-	
-}
 
-function showStaticAddress( address ) {
-	var mycenter=new google.maps.LatLng( 43.65960972192819, -79.39695954322815);
+/*
+ * Purpose: Shows google map of a the static address "address" on a <div> element with 
+ * id "id". 
+ * 
+ * Input Arguments:
+ * -id: id of div element on which we want to show the map. 
+ * -address: address of the location that we want to show. (for now we only use Latitude and longitude)
+ * 
+ * Output Arguments:
+ * -None.
+ */
+function showStaticAddress( id, address ) {
+	//var mycenter=new google.maps.LatLng( 43.65960972192819, -79.39695954322815);
 	var mapOptions = {
-	  zoom: 12,
-	  center: mycenter,
+	  zoom: 18,
+	  center: address.LatLng,
 	  disableDefaultUI: true,
 	  streetViewControl:true,
 	  mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	
-	var div = document.getElementById("map");
+	//setting up the container
+	var div = document.getElementById( id );
 	div.style.visibility = "visible";
 	var map = new google.maps.Map(div, mapOptions);
 	
 	
 	
-	//marker.setMap(map);
 	
+	//write directions for inside the building
 	var infowindow = new google.maps.InfoWindow({
 		content:"<div style='font-size:12px;overflow:auto'>Take Elevator to fifth floor, and " +
 				"look for a half circle in the main " +
@@ -38,7 +73,7 @@ function showStaticAddress( address ) {
 	
 	
 	var marker = new google.maps.Marker({
-		position:mycenter,
+		position:address.LatLng,
 		map: map,
 	});
 	
